cb1f1b3f72c3219882c1e75e31bc40e45f961418
Young Han Lee <joybro201@gmail.com>
2023-12-21 14:03:08 +0000

    Delete the old APIG
    Now we can safely delete this as no stack is referring to this.

--- cdk/lib/stacks/api-stack.ts
+++ cdk/lib/stacks/api-stack.ts
@@ -2,7 +2,6 @@
 import * as cdk from "aws-cdk-lib";
 import * as path from "path";
 import { Construct } from "constructs";
-import { ApiGatewayLambdaConstruct } from "../constructs/apigateway-lambda";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 
@@ -11,18 +10,6 @@ export class APIStack extends cdk.Stack {
 
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
-
-        // TODO - Remove this once the dependency is removed
-        // Instantiate the ApiGatewayLambdaConstruct
-        const oldApi = new ApiGatewayLambdaConstruct(this, "Counter", {
-            lambdaPath: path.resolve(__dirname, "../../lambdas/counter.ts"),
-            resourceName: "counter",
-            methods: ["GET", "POST"],
-        });
-        // Temporarily export the API Gateway ID and stage name to avoid deployment errors
-        // This will be removed once the dependency is removed.
-        this.exportValue(oldApi.api.restApiId);
-        this.exportValue(oldApi.api.deploymentStage.stageName);
 
         // Define the Lambda function
         const lambdaFunction = new NodejsFunction(this, `${id}-Handler`, {
