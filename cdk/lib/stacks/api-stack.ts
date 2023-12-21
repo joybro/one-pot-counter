6672d96237336e786296c1c3fc89278c0acdd7e1
Young Han Lee <joybro201@gmail.com>
2023-12-21 13:58:46 +0000

    Add /api resource to APIG
    This is to use the APIG with CloudFront because we will call CloudFront
    with /api path to call the APIG behind it.
    
    Also note that the old APIG is not deleted yet and its values are
    explicitly exported with exportValue method. This is to avoid deployment
    error as they are still referred by CloudFront stack.

--- cdk/lib/stacks/api-stack.ts
+++ cdk/lib/stacks/api-stack.ts
@@ -3,18 +3,47 @@ import * as cdk from "aws-cdk-lib";
 import * as path from "path";
 import { Construct } from "constructs";
 import { ApiGatewayLambdaConstruct } from "../constructs/apigateway-lambda";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 
 export class APIStack extends cdk.Stack {
-    api: ApiGatewayLambdaConstruct;
+    public readonly api: apigateway.RestApi;
 
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
+        // TODO - Remove this once the dependency is removed
         // Instantiate the ApiGatewayLambdaConstruct
-        this.api = new ApiGatewayLambdaConstruct(this, "Counter", {
+        const oldApi = new ApiGatewayLambdaConstruct(this, "Counter", {
             lambdaPath: path.resolve(__dirname, "../../lambdas/counter.ts"),
             resourceName: "counter",
             methods: ["GET", "POST"],
+        });
+        // Temporarily export the API Gateway ID and stage name to avoid deployment errors
+        // This will be removed once the dependency is removed.
+        this.exportValue(oldApi.api.restApiId);
+        this.exportValue(oldApi.api.deploymentStage.stageName);
+
+        // Define the Lambda function
+        const lambdaFunction = new NodejsFunction(this, `${id}-Handler`, {
+            entry: path.resolve(__dirname, "../../lambdas/counter.ts"),
+        });
+
+        // Define the API Gateway
+        this.api = new apigateway.RestApi(this, `${id}-ApiGateway`, {
+            restApiName: `${id}-Api`,
+        });
+
+        // Define the resources
+        const apiResource = this.api.root.addResource("api");
+        const counterResource = apiResource.addResource("counter");
+
+        // Add methods to the resource
+        ["GET", "POST"].forEach((method) => {
+            counterResource.addMethod(
+                method,
+                new apigateway.LambdaIntegration(lambdaFunction)
+            );
         });
     }
 }
